74f38a7fe5ece62ac1196b942b13d786cde49ece
forteller11 <charly.yan.miller@gmail.com>
2020-02-07 15:32:24 -0500

    Wrote first Unity ECS scripts

---
+++ Assets/Scripts/DotsTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEditor;
+using UnityEngine;
+
+public class DotsTest : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle data)
+    {
+        float t = Time.DeltaTime;
+        
+        JobHandle jh = Entities.ForEach((
+            ref Translation pos, in MoveBy moveBy) =>
+        {
+            pos.Value.xyz += moveBy.translation;
+        }).Schedule(data);
+
+        return jh;
+    }
+}
+[GenerateAuthoringComponent]
+public struct MoveBy : IComponentData
+{
+    public float3 translation; 
+}
