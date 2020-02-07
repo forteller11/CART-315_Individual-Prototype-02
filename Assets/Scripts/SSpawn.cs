dd456768564e625121d295b686a789fd3f6360e5
forteller11 <charly.yan.miller@gmail.com>
2020-02-07 17:21:05 -0500

    got entities rendering (very basic)

---
+++ Assets/Scripts/SSpawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+public class RandomMovement : ComponentSystem
+{
+    public Mesh Mesh;
+    public Material Material;
+    protected override void OnStartRunning()
+    {
+//        Entities.ForEach(out RenderMesh renderMesh) =>
+//        {
+//            renderMesh.material = new Material();
+//            renderMesh.mesh = Mesh;
+//        };
+    }
+
+    protected override void OnUpdate()
+    {
+//        Entities.ForEach(ref RenderMesh Translation) =>
+//        {
+//            renderMesh.material = new Material();
+//            renderMesh.mesh = Mesh;
+//        };
+    }
+}
---
+++ Assets/Scripts/SpawnEntities.cs
@@ -0,0 +1,43 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+
+public class SpawnEntities : MonoBehaviour
+{
+    public int NumberOfEntities = 100;
+    [SerializeField] public Mesh Mesh;
+    [SerializeField] public Material Material;
+    void Start()
+    {
+        var ecsManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        var archetype = ecsManager.CreateArchetype(
+            typeof(Translation),
+            typeof(Rotation),
+            typeof(RenderMesh),
+            typeof(LocalToWorld)
+        );
+        //ecsManager.SetE
+
+        NativeArray<Entity> entities = ecsManager.CreateEntity(archetype, NumberOfEntities, Allocator.Temp);
+
+        foreach (var entity in entities)
+        {
+            ecsManager.SetComponentData(entity, 
+                new Translation {Value = new float3(0,0,0.5f)}
+                );
+            
+            ecsManager.SetSharedComponentData(entity, 
+                new RenderMesh {mesh = Mesh, material = Material}
+            );
+        }
+        Debug.Log(entities);
+        Debug.Log(entities[0]);
+
+        entities.Dispose();
+    }
+    
+}
